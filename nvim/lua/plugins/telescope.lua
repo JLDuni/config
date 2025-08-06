f266fdf0bd3d6bdb7246c7a847b5b4a937635124
JLDuni <uc2023214314@student.uc.pt>
2025-08-07 00:02:30 +0100

    Update telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,28 +1,39 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>ft', builtin.current_buffer_fuzzy_find, {})
-    end
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    -- This is your opts table
-    config = function()
-    require("telescope").setup ({
-      extensions = {
-        ["ui-select"] = {
-          require("telescope.themes").get_dropdown {
-          },
-        }
-      }
-    })
-    require("telescope").load_extension("ui-select")
-  end
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>ft", builtin.current_buffer_fuzzy_find, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		-- This is your opts table
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-j>"] = require("telescope.actions").move_selection_next,
+							["<C-k>"] = require("telescope.actions").move_selection_previous,
+						},
+						n = {
+							["<C-j>"] = require("telescope.actions").move_selection_next,
+							["<C-k>"] = require("telescope.actions").move_selection_previous,
+						},
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
-
