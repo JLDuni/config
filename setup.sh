efc5bdbb1f85f8461e7094e48d41e05388551ae1
JLDuni <uc2023214314@student.uc.pt>
2026-03-31 16:12:27 +0100

    mes

--- setup.sh
+++ setup.sh
@@ -25,25 +25,3 @@ AUR_PKGS=(
 )
 
 yay -S --needed "${OFFICIAL_PKGS[@]}" "${AUR_PKGS[@]}" --noconfirm
-
-# --- 4. Deploy Dotfiles (Bare Git Method) ---
-# Replace 'username' and 'dotfiles' with your actual GitHub info
-DOTFILES_REPO="https://github.com/username/dotfiles.git"
-
-echo "Deploying dotfiles from GitHub..."
-git clone --bare $DOTFILES_REPO $HOME/.cfg
-
-# Define the alias locally for the remainder of the script
-function config {
-   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
-}
-
-# Backup existing configs just in case
-mkdir -p .config-backup
-config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-
-# Checkout the files
-config checkout
-config config --local status.showUntrackedFiles no
-
-echo "Setup complete! Please reboot to ensure all groups (like vboxusers) and services are active."
