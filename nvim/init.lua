51c9b0c6218c83bde560605173ed1e4781c0243c
JLDuni <uc2023214314@student.uc.pt>
2026-04-11 18:34:22 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,15 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
 --lazy
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46_cache/"
+
 require("config.lazy")
+
+for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
+  dofile(vim.g.base46_cache .. v)
+end
+
+vim.opt.showtabline = 0
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -14,16 +22,16 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#6c7086" })
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#f38ba8", bold = true })
+vim.keymap.set("i", "<C-z>", "<lt>")
 
 vim.opt.clipboard = "unnamedplus"
 vim.opt.hlsearch = false
 
-vim.opt.mouse = ""
-
-vim.opt.scrolloff = 8
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 vim.keymap.set("v", "L", ">gv")
 vim.keymap.set("v", "H", "<gv")
+
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.scrolloff = 8
